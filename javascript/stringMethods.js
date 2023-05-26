b2d85033cae7326d811dffcf1ebc22356b8ba57c
Marco Alarcon <alarconm96@gmail.com>
2023-05-26 15:50:22 -0400

    Add various string functions for testing

--- javascript/stringMethods.js
+++ javascript/stringMethods.js
@@ -1,4 +1,39 @@
 const myString = "Hello World!";
+const money = 12;
 
 //display string length
-document.getElementById("string-length").innerText = myString.length;
+function getLength() {
+    document.getElementById("string-length").innerText = myString.length;
+}
+
+//front slice
+function getFrontSlice() {
+    document.getElementById("string-slice-front").innerText = myString.slice(0, 5);
+}
+
+//back slice
+function getBackSlice() {
+    document.getElementById("string-slice-back").innerText = myString.slice(6);
+}
+
+//replace word
+function replaceSubstring() {
+    document.getElementById("string-replace").innerText = myString.replace("Hello", "Goodbye");
+}
+
+//concat string
+function concatString() {
+    document.getElementById("string-concat").innerText = myString.concat(" ", "And ", "Goodnight!");
+}
+
+//split hello
+function splitHello(){
+    const newHello = myString.split(" ");
+    document.getElementById("string-split").innerText = newHello[1] + " " + newHello[0];
+}
+
+//interpolate hello
+function interpolate(){
+    const newString = `This string uses both "double-quotes" and 'single-quotes'`;
+    document.getElementById("string-interpolate").innerText = `Hello ${newString} World!`;
+}
