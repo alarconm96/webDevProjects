b9a8db2dd0e33f396bf89b43dd9b97bb2e3c8325
Marco Alarcon <alarconm96@gmail.com>
2023-05-30 17:04:05 -0400

    Add function to arrayTesting.js to set 'arr-toString' HTML button innerText to array in index.html

--- javascript/arrayTesting.js
+++ javascript/arrayTesting.js
@@ -1,6 +1,11 @@
 //set up array to hold fruits
 const myArr = ['apple', 'orange', 'banana', 'mango', 'grape'];
 
+//function to set innertext to array as string
+function getArr(){
+    document.getElementById('arr-toString').innerText = myArr.toString();
+}
+
 //function to set innertext to element at index i
 function getElem(i, id) {
     document.getElementById(`${id}`).innerText = myArr[i];
