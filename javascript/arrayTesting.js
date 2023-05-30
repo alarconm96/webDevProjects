969f672770b590daea6f9c9940f75ba929f0c9a7
Marco Alarcon <alarconm96@gmail.com>
2023-05-30 17:08:56 -0400

    Create function to test displaying full array compared  to myArr.toString()

--- javascript/arrayTesting.js
+++ javascript/arrayTesting.js
@@ -6,6 +6,10 @@ function getArr(){
     document.getElementById('arr-toString').innerText = myArr.toString();
 }
 
+function getFullArr(){
+    document.getElementById('arr-fullArr').innerText = myArr;
+}
+
 //function to set innertext to element at index i
 function getElem(i, id) {
     document.getElementById(`${id}`).innerText = myArr[i];
